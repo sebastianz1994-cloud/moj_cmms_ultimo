44acf8d409e69c6b60204b035cc33be6f2f1626c
sebastianz1994-cloud <sebastianz1994@gmail.com>
2026-06-07 18:50:27 +0200

    Automatyczny deploy strony WWW

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1706915348" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "624724358" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
